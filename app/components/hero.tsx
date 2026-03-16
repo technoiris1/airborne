9665c9869e08e4500cdf2ccf7d634b1a03305259
Manan Sharma <83415076+technoiris1@users.noreply.github.com>
2026-03-16 22:00:34 +0530

    added a basic hero

---
+++ app/components/hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <img
+        src="/hero.png"
+        alt="Hero"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+    </section>
+  );
+}
