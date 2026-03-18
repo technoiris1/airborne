b554d1e0510b421a1d008dddb92ab8afa458b0af
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-03-18 14:49:42 +0530

    small tweak

--- app/components/hack-club-banner.tsx
+++ app/components/hack-club-banner.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Banner() {
   return (
-    <Link href={"hackclub.com"}>
+    <Link href={"https://hackclub.com/"} target="_blank">
       <img
         className="absolute top-5 left-[-1px] w-[256px] z-[999]"
         src="/banner.svg"
