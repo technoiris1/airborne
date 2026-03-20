b14200f9685550fcf05dd9f8a54512e2814d5931
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-03-20 22:38:34 +0530

    did smth

--- app/components/desc.tsx
+++ app/components/desc.tsx
@@ -7,8 +7,7 @@ const outfit = Outfit({
 
 export default function Description() {
   return (
-    <section className="w-full h-screen relative flex items-center justify-center">
-      <div className="absolute inset-0 bg-white/10 backdrop-blur-md border-t border-white/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]" />
+    <section className="w-full h-screen relative flex items-center justify-center bg-white/20 backdrop-blur-xl border border-white/30">
       <div
         className={`${outfit.className} text-2xl relative z-10 max-w-6xl mx-auto px-8 text-black`}
       >
--- app/components/machines.tsx
+++ app/components/machines.tsx
@@ -9,10 +9,10 @@ export default function Machines() {
   return (
     <section className="w-full h-screen relative flex items-center justify-center">
       <div className="absolute inset-0 bg-white/10 backdrop-blur-md border-t border-white/20 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]" />
-      <div
-        className={`${outfit.className} text-2xl relative z-10 max-w-6xl mx-auto px-8 text-black`}
-      >
-        HEYYY NEHAHAHAHAHAAHHAHAH
+      <div className="z-2000">
+        <div className="h-120 w-80 border border-white/90 flex justify-center p-4 bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0">
+          <div className="h-40 w-80 border border-white/80"></div>
+        </div>
       </div>
     </section>
   );
