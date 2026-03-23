8c178d6f570682a2f4929d371634705a392b0d1f
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-03-24 00:51:11 +0530

    added smooth scroll

---
+++ app/components/LenisScroll.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function LenisScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.3,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -11 * t)),
+      direction: "vertical",
+      gestureDirection: "vertical",
+      smooth: true,
+      mouseMultiplier: 0.9,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+}
